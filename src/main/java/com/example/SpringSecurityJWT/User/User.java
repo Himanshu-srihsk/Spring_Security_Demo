b3545b97d1c1683c0ffbc09493728bdfbb635b05
Kumar Himanshu (CSS ICW ENG SW BTFW SVT) <Himanshu.Kumar@infineon.com>
2024-04-19 21:55:37 +0530

    Added Spring Security Demo Project

---
+++ src/main/java/com/example/SpringSecurityJWT/User/User.java
@@ -0,0 +1,85 @@
+package com.example.SpringSecurityJWT.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails{
+	 @Id
+	    @GeneratedValue
+	 private Integer id;
+
+	    private String firstName;
+
+	    private String lastName;
+
+	    private String email;
+
+	    private String password;
+	    
+	    @Enumerated(EnumType.STRING)
+	    private Role role;
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			// TODO Auto-generated method stub
+			return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
+		}
+
+
+		@Override
+		public String getUsername() {
+			// TODO Auto-generated method stub
+			return email;
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			// TODO Auto-generated method stub
+			return true;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			// TODO Auto-generated method stub
+			return true;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			// TODO Auto-generated method stub
+			return true;
+		}
+
+		@Override
+		public boolean isEnabled() {
+			// TODO Auto-generated method stub
+			return true;
+		}
+}
